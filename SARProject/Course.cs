06b43d47b823b602dc530ad396386f585ba733c8
iFialAtFail <mmanle01@gmail.com>
2016-10-04 11:00:43 -0400

    Cleaned up properties for course class

--- SARProject/Course.cs
+++ SARProject/Course.cs
@@ -12,54 +12,15 @@ namespace StudentDataXMLGenerator
 
         #endregion
 
-        #region Private Fields
-
-        private string id;
-        private string name;
-        private int credits;
-        private string semester;
-        private string courseType;
-        private string grade;
-
-        #endregion
-
         #region Public Properties
 
-        public string ID
-        {
-            get { return id; }
-            set { id = value; }
-        }
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public string Semester { get; set; }
+        public string CourseType { get; set; }
+        public string Grade { get; set; }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public int Credits
-        {
-            get { return credits; }
-            set { credits = value; }
-        }
-
-        public string Semester
-        {
-            get { return semester; }
-            set { semester = value; }
-        }
-
-        public string CourseType
-        {
-            get { return courseType; }
-            set { courseType = value; }
-        }
-
-        public string Grade
-        {
-            get { return grade; }
-            set { grade = value; }
-        }
 
         #endregion
 
